6fcd7cab7bcaedd9b61f354a0ef53b1a37b01f3b
贾子康 <6564690+jia_zi_kang@user.noreply.gitee.com>
2024-05-16 15:41:10 +0800

    first

--- .leetcode/141.环形链表.cpp
+++ .leetcode/141.环形链表.cpp
@@ -1,3 +1,16 @@
+// @before-stub-for-debug-begin
+#include <vector>
+#include <string>
+
+
+using namespace std;
+// @before-stub-for-debug-end
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
 /*
  * @lc app=leetcode.cn id=141 lang=cpp
  *
@@ -18,11 +31,26 @@
      int val;
     ListNode *next;
    ListNode(int x) : val(x), next(NULL) {}
+  };
  
  
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        int i =1;
+        ListNode* p=head;
+        // if(head==nullptr||head->next==nullptr)//空表和单节点表无环
+        //     return false;
+        while (p!=nullptr)
+        {
+            p=p->next;
+            i++;
+            if(i==10002)//如果能够循环10001次以上，说明存在环
+               return true;
+        }
+        return false;
+            
+
         
     }
 };
