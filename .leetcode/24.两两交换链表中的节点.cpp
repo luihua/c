e3830493a4d4022fc23d1235eaaaf25c4df943b0
贾子康 <6564690+jia_zi_kang@user.noreply.gitee.com>
2024-06-06 20:32:43 +0800

    f

--- .leetcode/24.两两交换链表中的节点.cpp
+++ .leetcode/24.两两交换链表中的节点.cpp
@@ -1,3 +1,8 @@
+
+/*************************************************************************************************/
+//每次交换遍历节点的后两个节点，之后将遍历节点后移两位dump->1->2->..，dump->2->1->..,如果节点为单数，p1->next.next为null停止，节点为偶数，p1.next=null停止
+/*************************************************************************************************/
+
 /*
  * @lc app=leetcode.cn id=24 lang=cpp
  *
@@ -26,6 +31,20 @@ struct ListNode {
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
+        ListNode *h1,*p1,*temp;
+        h1=new ListNode(0,head);
+        p1=h1;
+        while(p1->next!=nullptr&&p1->next->next!=nullptr)
+        {
+            temp=p1->next;
+            p1->next=temp->next;
+            temp->next=p1->next->next;
+            p1->next->next=temp;
+            p1=p1->next->next;
+        }
+    ListNode *ans=h1->next;
+    delete h1;
+    return ans;
 
     }
 };
---
+++ .leetcode/61.旋转链表.cpp
@@ -0,0 +1,84 @@
+/*
+ * @lc app=leetcode.cn id=61 lang=cpp
+ *
+ * [61] 旋转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// struct ListNode {
+//       int val;
+//       ListNode *next;
+//       ListNode() : val(0), next(nullptr) {}
+//       ListNode(int x) : val(x), next(nullptr) {}
+//       ListNode(int x, ListNode *next) : val(x), next(next) {}
+//   };
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+     ListNode *h1,*p1,*p2,*ans;
+     h1=new ListNode(0,head);
+     p1=h1;
+     p2=h1;
+     int n=0;
+     while (p2->next!=nullptr)
+     {
+        p2=p2->next;
+        n++;
+        
+     }
+     if(n==1||n==0)
+     {
+        ans=h1->next;
+        // delete h1;
+        return ans;
+     }
+     p2->next=h1->next;
+     n=k%n; 
+     for (int i = 0; i < n; i++)
+     {
+        p2=p2->next;
+     }
+     while (p2->next!=p1->next)
+     {
+        h1=h1->next;
+        p2=p2->next;
+     }
+    
+
+
+ans=h1->next;
+h1->next=nullptr;
+return ans;
+}
+     
+};
+// @lc code=end
+
+//*******************************************int main()*********************************************/
+/**************************************************************************************************/
+int main()
+{
+    Solution S;
+    ListNode *head=nullptr,*ans;
+    // head=new ListNode(1);
+    // head->next=new ListNode(2);
+    // head->next->next= new ListNode(3);
+    // head->next->next->next = new ListNode(4);
+    // head->next->next->next->next = new ListNode(5);
+    // head->next->next->next->next->next = new ListNode(6);
+    // head->next->next->next->next->next->next= new ListNode(7);
+    // head->next->next->next->next->next->next->next= new ListNode(8);
+    ans=S.rotateRight(head,2);
+    return 0;
+    
+}
